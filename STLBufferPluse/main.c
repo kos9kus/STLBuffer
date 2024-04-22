1c5ddd6bd75744494116eb87a91d0e56a13eddd7
Konstantin Kusainov <konstantin.kusainov@btsdigital.kz>
2024-04-23 00:11:22 +0300

    Initial Commit

---
+++ STLBufferPluse/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  STLBufferPluse
+//
+//  Created by Kos on 23.04.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
